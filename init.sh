4f803ebfdb2a8aa622b19806241780174f880d08
NikkyAI <root@nikky.moe>
2018-08-20 15:43:14 +0200

    fix changed location of jar in init script

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo updating voodoo bootstrap
 
-curl -o bootstrap-voodoo.jar  https://ci.elytradev.com/job/elytra/job/Voodoo/job/rewrite/lastSuccessfulBuild/artifact/bootstrap/build/libs/bootstrap-voodoo-53.jar
+curl -o bootstrap-voodoo.jar https://ci.elytradev.com/job/elytra/job/Voodoo/job/rewrite/60/artifact/bootstrap/build/libs/bootstrap-voodoo-60.jar
 
