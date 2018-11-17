6ea91e5db81dfb4f9883c8bb8b5fca471d6a0fc7
NikkyAI <root@nikky.moe>
2018-11-18 00:34:01 +0100

    update voodoo to 0.4.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     wrapper
-    id("voodoo") version "0.4.3-SNAPSHOT"
+    id("voodoo") version "0.4.4-SNAPSHOT"
 }
 
 voodoo {
     addTask(name = "build", parameters = listOf("build"))
     addTask(name = "sk", parameters = listOf("pack sk"))
-    addTask(name = "packServer", parameters = listOf("pack server"))
+    addTask(name = "server", parameters = listOf("pack server"))
     addTask(name = "buildAndPackAll", parameters = listOf("build", "pack sk", "pack server", "pack mmc"))
 }
 
